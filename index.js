b7e14fd3e8f748b5cf076811ead03ea45668b800
Daniel Bonn <danieltbonn@gmail.com>
2023-08-15 19:42:55 -0500

    created questions using the inquirer package

--- index.js
+++ index.js
@@ -6,13 +6,41 @@ questions = [
     {
         type: 'input',
         name: 'logo',
-        message: 'Enter up to three characters to generate your logo',
+        message: 'Enter up to three characters to generate your logo.',
         validate(value) {
             if (value.length <= 3 && value.length > 1) {
                 return true;
             }
             return "You must enter up to three characters"
         }
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter a color for the text using a keyword or hexadecimal (i.e. red or #FF0000).',
+        validate(value) {
+            // if (value.length <= 3 && value.length > 1) {
+            //     return true;
+            // }
+            return "You must enter a valid color"
+        }
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Please choose a shape for your logo.',
+        choices = ['circle', 'triangle', 'square']
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter a color for the shape using a keyword or hexadecimal (i.e. red or #FF0000).',
+        validate(value) {
+            // if (value.length <= 3 && value.length > 1) {
+            //     return true;
+            // }
+            return "You must enter a valid color"
+        },
     }
 ]
 
@@ -30,4 +58,4 @@ function init() {
     });
 }
 
-init()
+init();
